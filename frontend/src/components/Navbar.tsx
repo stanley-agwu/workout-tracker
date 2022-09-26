3614f317e04fb70e85fccf37a2eb61a5ffef1329
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2022-09-26 12:27:13 +0200

    Added NavBar, routes and route

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+const NavBar: React.FC = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="sm">
+      <Container fluid>
+        <Navbar.Brand href="/">Freak workouts</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="#action2">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
