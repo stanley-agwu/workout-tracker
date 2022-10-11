d042a51fa044c6c3e7a07aad086af2c5eadf618f
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2022-10-11 13:00:35 +0200

    weShip MERN - frontend testing - created test for rendering of login page on initial rendering of app

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { AuthContextProvider } from './context/AuthContext';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('signin form rendered on initial loading of component', () => {
+  test('renders signin form - empty', () => {
+    render(
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    );
+    const header = screen.getByRole('heading', { name: /sign in to freak workouts/i });
+    const emailInputField = screen.getByRole('textbox', { name: /username or email address/i });
+    const passwordInputField = screen.getByLabelText(/password/i);
+    const signInButton = screen.getByRole('button', { name: /sign in/i });
+    expect(header).toBeInTheDocument();
+    expect(emailInputField).toHaveDisplayValue('');
+    expect(passwordInputField).toHaveDisplayValue('');
+    expect(signInButton).toBeInTheDocument();
+
+  });
+})
